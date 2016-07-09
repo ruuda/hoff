bd89913d8e1f523465bf1e44da43bd7698f78988
Ruud van Asseldonk <dev@veniogames.com>
2016-07-09 14:30:29 +0200

    Add Cabal file and empy main

---
+++ src/Main.hs
@@ -0,0 +1,10 @@
+-- Copyright 2016 Ruud van Asseldonk
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License version 3. See
+-- the licence file in the root of the repository.
+
+module Main where
+
+main :: IO ()
+main = putStrLn "not yet implemented"
