af8aa4bb080a59c08aa03aaddeb0bf2ff038bdc1
Ruud van Asseldonk <dev@veniogames.com>
2016-07-17 12:46:35 +0200

    Add a basic router to the server

--- src/Server.hs
+++ src/Server.hs
@@ -8,16 +8,37 @@
 
 module Server (runServer) where
 
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types (status200, status404)
 import Network.HTTP.Types.Header (hContentType)
-import Network.Wai (Application, responseLBS)
+import Network.Wai (Application, responseLBS, pathInfo)
 import Network.Wai.Handler.Warp (run)
 
-webhookListener :: Application
-webhookListener request f =
-  f $ responseLBS status200 [(hContentType, "text/plain")] "hi"
+-- Router for the web server:
+--  * The GitHub webhook endpoint is at /hook/github.
+--  * The webinterface is at /.
+router :: Application
+router request = case pathInfo request of
+  "hook" : "github" : [] -> githubWebhook request
+  []                     -> webInterface request
+  otherpath              -> notFound request
 
+-- Serves the GitHub webhook endpoint.
+githubWebhook :: Application
+githubWebhook request f =
+  f $ responseLBS status200 [(hContentType, "text/plain")] "hook received"
+
+-- Serves the webinterface.
+webInterface :: Application
+webInterface request f =
+  f $ responseLBS status200 [(hContentType, "text/plain")] "not yet implemented"
+
+-- Fallback if no route matched.
+notFound :: Application
+notFound request f =
+  f $ responseLBS status404 [(hContentType, "text/plain")] "not found"
+
+-- Runs a webserver at the specified port.
 runServer :: Int -> IO ()
 runServer port = do
   putStrLn $ "Listening for webhooks on port " ++ (show port)
-  run port webhookListener
+  run port router
