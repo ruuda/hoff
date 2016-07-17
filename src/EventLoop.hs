668a2e581f123dd8d894624c98aa6c0b0d33546c
Ruud van Asseldonk <dev@veniogames.com>
2016-07-17 21:13:22 +0200

    Implement conversion of comment webhook event

--- src/EventLoop.hs
+++ src/EventLoop.hs
@@ -11,30 +11,43 @@ module EventLoop (runGitHubEventLoop) where
 
 import Control.Concurrent.STM.TBQueue
 import Control.Monad.STM (atomically)
+
+import GitHub (PullRequestPayload, PullRequestCommentPayload, WebhookEvent (..))
 import Project (PullRequestId (..))
 
 import qualified GitHub
 import qualified Logic
 
-eventFromPullRequestPayload :: GitHub.PullRequestPayload -> Logic.Event
+eventFromPullRequestPayload :: PullRequestPayload -> Logic.Event
 eventFromPullRequestPayload payload =
-  let number = GitHub.number (payload :: GitHub.PullRequestPayload) -- TODO: Use PullRequestId wrapper from beginning.
-      author = GitHub.author (payload :: GitHub.PullRequestPayload) -- TODO: Wrapper type
-      sha    = GitHub.sha    (payload :: GitHub.PullRequestPayload)
-  in case GitHub.action (payload :: GitHub.PullRequestPayload) of
+  let number = GitHub.number (payload :: PullRequestPayload) -- TODO: Use PullRequestId wrapper from beginning.
+      author = GitHub.author (payload :: PullRequestPayload) -- TODO: Wrapper type
+      sha    = GitHub.sha    (payload :: PullRequestPayload)
+  in case GitHub.action (payload :: PullRequestPayload) of
     GitHub.Opened      -> Logic.PullRequestOpened (PullRequestId number) sha author
     GitHub.Reopened    -> Logic.PullRequestOpened (PullRequestId number) sha author
     GitHub.Closed      -> Logic.PullRequestClosed (PullRequestId number)
     GitHub.Synchronize -> Logic.PullRequestCommitChanged (PullRequestId number) sha
 
-eventFromPullRequestCommentPayload :: GitHub.PullRequestCommentPayload -> Logic.Event
-eventFromPullRequestCommentPayload payload = undefined
+eventFromPullRequestCommentPayload :: PullRequestCommentPayload -> Maybe Logic.Event
+eventFromPullRequestCommentPayload payload =
+  let number = GitHub.number (payload :: PullRequestCommentPayload) -- TODO: Use PullRequestId wrapper from beginning.
+      author = GitHub.author (payload :: PullRequestCommentPayload) -- TODO: Wrapper type
+      body   = GitHub.body   (payload :: PullRequestCommentPayload)
+  in case GitHub.action (payload :: PullRequestCommentPayload) of
+    GitHub.Created -> Just $ Logic.CommentAdded (PullRequestId number) author body
+    -- Do not bother with edited and deleted comments, as it would tremendously
+    -- complicate handling of approval. Once approved, this cannot be undone.
+    -- And if approval undo is desired, it would be better implemented as a
+    -- separate magic comment, rather than editing the approval comment.
+    GitHub.Edited  -> Nothing
+    GitHub.Deleted -> Nothing
 
 convertGitHubEvent :: GitHub.WebhookEvent -> Maybe Logic.Event
 convertGitHubEvent event = case event of
-  GitHub.Ping                       -> Nothing -- TODO: What to do with this one?
-  GitHub.PullRequest payload        -> Just $ eventFromPullRequestPayload payload
-  GitHub.PullRequestComment payload -> Just $ eventFromPullRequestCommentPayload payload
+  Ping                       -> Nothing -- TODO: What to do with this one?
+  PullRequest payload        -> Just $ eventFromPullRequestPayload payload
+  PullRequestComment payload -> eventFromPullRequestCommentPayload payload
 
 runGitHubEventLoop :: GitHub.EventQueue -> IO ()
 runGitHubEventLoop ghQueue = do
