82ebdb4f10eaf34737c3710ad6f7e2621143c7ce
Ruud van Asseldonk <dev@veniogames.com>
2016-08-09 00:25:16 +0200

    Implement 'not found' server test

--- tests/ServerSpec.hs
+++ tests/ServerSpec.hs
@@ -15,7 +15,11 @@
 module ServerSpec (serverSpec) where
 
 import Control.Concurrent (forkIO, killThread)
-import Test.Hspec
+import Control.Lens (set, view)
+import Data.ByteString.Lazy (ByteString)
+import Test.Hspec (Spec, describe, it, parallel, shouldBe)
+
+import qualified Network.Wreq as Wreq
 
 import Server (runServer)
 
@@ -23,6 +27,18 @@ import qualified Github
 
 testPort :: Int
 testPort = 5273
+
+testHost :: String
+testHost = "http://localhost:" ++ (show testPort)
+
+-- The normal Wreq.get function throws when the response is a 404, which is not
+-- what we want. The solution is to use custom options, with the 'checkStatus'
+-- function set to one that does not throw for non-200 statuses.
+httpGet :: String -> IO (Wreq.Response ByteString)
+httpGet = Wreq.getWith options
+  where
+    ignoreStatus _ _ _ = Nothing
+    options = set Wreq.checkStatus (Just ignoreStatus) Wreq.defaults
 
 withServer :: (Github.EventQueue -> IO ()) -> IO ()
 withServer body = do
@@ -41,5 +57,6 @@ serverSpec = parallel $ do
 
     it "serves 'not found' at a non-existing url" $
       withServer $ \ _ghQueue -> do
-        -- TODO: Send an actual request, inspect the response.
-        True `shouldBe` True
+        response <- httpGet $ testHost ++ "/bogus/url"
+        let statusCode = view (Wreq.responseStatus . Wreq.statusCode) response
+        statusCode `shouldBe` 404
