39c868d2d089010e831a9394dd95141087b31b3a
Ruud van Asseldonk <dev@veniogames.com>
2016-08-31 22:44:43 +0200

    Add script to do a release build

---
+++ package/build-binary.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# This script invokes Stack to do a release build. It is intended to be run on
+# on the same system that packages the binaries, right before build-package.sh.
+
+# Fail early of any of the commands below fail.
+set -e
+
+# Download the right version of GHC.
+stack setup
+
+# When running in a virtual machine, if we inherited a dirty Stack working
+# directory from the host machine, be sure to clean it.
+stack clean
+
+# Build with --split-objs to produce a smaller binary, and enable optimizations.
+stack build --split-objs --ghc-options -O2
+
+# Finally run the tests to make sure that everything still works.
+stack test --split-objs --ghc-options -O2
--- package/build-package.sh
+++ package/build-package.sh
@@ -5,8 +5,12 @@
 #
 #   Usage: VERSION=0.0.0 ./build-package.sh
 
-# Set package metadata.
+
+# Set package filename.
 PKGNAME="hoff_$VERSION-1"
+
+# Fail early of any of the commands below fail.
+set -e
 
 # Recreate the file system layout as it should be on the target machine.
 mkdir -p "$PKGNAME/DEBIAN"
@@ -18,3 +22,6 @@ cp "$(stack path --local-install-root)/bin/hoff" "$PKGNAME/usr/bin/"
 cat deb-control | envsubst > "$PKGNAME/DEBIAN/control"
 
 dpkg-deb --build "$PKGNAME"
+
+# Finally clean up the package directory.
+rm -fr "$PKGNAME"
