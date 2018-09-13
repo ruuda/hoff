a7cbd7d9fc8bbf480b8f61e2e306bd85ede504e5
Ruud van Asseldonk <dev@veniogames.com>
2018-09-13 22:18:17 +0200

    Add Nix package definition
    It is broken at this point though, not sure why, it just exits in the
    "configure" phase without any output.

---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/56b9f6fc8e1c3a4ad10ff7c61e461d7b7e038833.tar.gz";
+  sha256 = "0v5y4wjfxbappsaibw88h7n1flcx7kpvg51mjv3h9m4aa3fn2c8q";
+}
