afb86f45313b8a15041aaf7e98b961a8998bdbb3
Ruud van Asseldonk <dev@veniogames.com>
2018-09-15 00:43:55 +0200

    Make building with Nix work
    We can build Nix with Stack, invoked by Nix, invoked by Stack, invoked
    by Nix. Could we go deeper?
    
    On a more serious note, this is really nice. One command to rule them
    all, a "nix build" to get a reproducible binary, with *all* dependencies
    pinned through files under source control, and the one command will go
    all the way and spit out what I want.
    
    I intend to go further, and make the target not a binary, but a squashfs
    file system of the closure that would allow me to deploy Hoff with
    systemd portable services. The systemd container empire is real.

--- default.nix
+++ default.nix
@@ -14,10 +14,43 @@ let
     sha256 = "0dnwv3yfmfncabqgspin1dshiaykbqh3iymn7y6d048fmlkdf272";
   }) { inherit lib; };
 
-  hoff = haskell.lib.buildStackProject {
+  # The Stackage snapshot that Hoff depends on, precompiled.
+  hoffDeps = stdenv.mkDerivation {
+    name = "hoff-deps";
+    buildInputs = [ stack nix ];
+    phases = [ "unpackPhase" "buildPhase" ];
+    # Take only the input files we need to get Stack going, and those that Stack
+    # needs to determine what to build from the snapshot. This target needs to
+    # be rebuilt every time one of the inputs changes, so we'd like to avoid
+    # rebuilding as much as possible.
+    unpackPhase = ''
+      cp ${./hoff.cabal} hoff.cabal
+      cp ${./nixpkgs-pinned.nix} nixpkgs-pinned.nix
+      cp ${./nixpkgs} nixpkgs
+      cp ${./shell.nix} shell.nix
+      cp ${./stack.yaml} stack.yaml
+    '';
+    buildPhase = ''
+      stack build --nix --stack-root $out --only-snapshot --test
+    '';
+  };
+
+  hoff = stdenv.mkDerivation {
     name = "hoff";
     src = gitignore.gitignoreSource ./.;
-    buildInputs = [ git ];
+    buildInputs = [ git stack nix hoffDeps ];
+    phases = [ "unpackPhase" "buildPhase" "checkPhase" "installPhase" ];
+    doCheck = true;
+    buildPhase = ''
+      stack build --nix --stack-root ${hoffDeps}
+    '';
+    checkPhase = ''
+      stack test --nix --stack-root ${hoffDeps}
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      stack install --nix --stack-root ${hoffDeps} --local-bin-path $out/bin
+    '';
   };
 
 in
