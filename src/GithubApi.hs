9388573c2a6f3124e453f94283f2d4a7f3af3ed5
Ruud van Asseldonk <ruud@channable.com>
2020-01-09 14:04:37 +0100

    Implement authentication check through GitHub API

--- src/GithubApi.hs
+++ src/GithubApi.hs
@@ -22,7 +22,7 @@ where
 
 import Control.Monad.Free (Free, liftF)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Logger (MonadLogger, logInfoN, logWarnN)
+import Control.Monad.Logger (MonadLogger, logDebugN, logInfoN, logWarnN, logErrorN)
 import Data.Text (Text)
 
 import qualified Data.Text as Text
@@ -30,6 +30,7 @@ import qualified Data.Text as Text
 import qualified GitHub.Data.Id as Github3
 import qualified GitHub.Data.Name as Github3
 import qualified GitHub.Endpoints.Issues.Comments as Github3
+import qualified GitHub.Endpoints.Repos.Collaborators as Github3
 
 import Project (ProjectInfo)
 import Types (PullRequestId (..), Username (..))
@@ -48,6 +49,13 @@ leaveComment pr remoteBranch = liftF $ LeaveComment pr remoteBranch ()
 
 hasPushAccess :: Username -> GithubOperation Bool
 hasPushAccess username = liftF $ HasPushAccess username id
+
+isPermissionToPush :: Github3.CollaboratorPermission -> Bool
+isPermissionToPush perm = case perm of
+  Github3.CollaboratorPermissionAdmin -> True
+  Github3.CollaboratorPermissionWrite -> True
+  Github3.CollaboratorPermissionRead -> False
+  Github3.CollaboratorPermissionNone -> False
 
 runGithub
   :: MonadIO m
@@ -70,6 +78,29 @@ runGithub auth projectInfo operation =
         Right _ -> logInfoN $ Text.concat ["Posted comment on ", Text.pack $ show pr, ": ", body]
       pure cont
 
-    HasPushAccess (Username _username) cont -> do
-      logInfoN "TODO: Make actual API call."
-      pure $ cont False
+    HasPushAccess (Username username) cont -> do
+      result <- liftIO $ Github3.collaboratorPermissionOn
+        (Just auth)
+        (Github3.N $ Project.owner projectInfo)
+        (Github3.N $ Project.repository projectInfo)
+        (Github3.N username)
+
+      case result of
+        Left err -> do
+          logErrorN
+            $ Text.append "Failed to retrive collaborator status: "
+            $ Text.pack $ show err
+          -- To err on the safe side, if the API call fails, we pretend nobody
+          -- has push access.
+          pure $ cont False
+
+        Right (Github3.CollaboratorWithPermission _user perm) -> do
+          logDebugN $ Text.concat
+            [ "User "
+            , username
+            , " has permission "
+            , Text.pack $ show perm
+            , " on "
+            , Text.pack $ show projectInfo
+            ]
+          pure $ cont $ isPermissionToPush perm
