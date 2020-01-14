8d9285893e3cb3f2e6cad0cc345ed6113a1ea35f
Ruud van Asseldonk <ruud@channable.com>
2020-01-14 16:04:36 +0100

    Use writer monad instead of tuple instance
    This is a bit more explicit, the tuple instance is a bit obscure. A
    writer monad is really what we want, to record a trace of what happened.

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -10,12 +10,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad.Free (foldFree)
+import Control.Monad.Trans.Writer (Writer, tell, runWriter)
 import Data.Aeson (decode, encode)
 import Data.ByteString.Lazy (readFile)
 import Data.Foldable (foldlM)
 import Data.Maybe (fromJust, isJust, isNothing)
 import Data.Text (Text)
-import Data.Tuple (swap)
 import Prelude hiding (readFile)
 import System.Directory (getTemporaryDirectory, removeFile)
 import System.FilePath ((</>))
@@ -69,25 +69,30 @@ data ActionFlat
 -- together with a list of all actions that would have been performed. Some
 -- actions require input from the outside world. Simulating these actions will
 -- return the pushResult and rebaseResult passed in here.
-runActionWithInit :: Maybe Sha -> PushResult -> Action a -> ([ActionFlat], a)
+runActionWithInit :: Maybe Sha -> PushResult -> Action a -> Writer [ActionFlat] a
 runActionWithInit integrateResult pushResult =
   let
     -- In the tests, only "deckard" is a reviewer.
     isReviewer username = username == "deckard"
   in
-    -- This relies on the monad instance `Monoid a => Monad ((,) a)`, which
-    -- relies on the monoid instance of list. At every step, we get to provide
-    -- a list, and the results get concatenated.
     foldFree $ \case
-      TryIntegrate msg candi h      -> ([ATryIntegrate msg candi], h integrateResult)
-      TryPromote prBranch headSha h -> ([ATryPromote prBranch headSha], h pushResult)
-      LeaveComment pr body x        -> ([ALeaveComment pr body], x)
-      IsReviewer username h         -> ([AIsReviewer username], h $ isReviewer username)
+      TryIntegrate msg candi h -> do
+        tell [ATryIntegrate msg candi]
+        pure $ h integrateResult
+      TryPromote prBranch headSha h -> do
+        tell [ATryPromote prBranch headSha]
+        pure $ h pushResult
+      LeaveComment pr body x -> do
+        tell [ALeaveComment pr body]
+        pure x
+      IsReviewer username h -> do
+        tell [AIsReviewer username]
+        pure $ h $ isReviewer username
 
 -- Simulates running the action. Pretends that integration always conflicts.
 -- Pretends that pushing is always successful.
 runAction :: Action a -> (a, [ActionFlat])
-runAction = swap . runActionWithInit Nothing PushOk
+runAction = runWriter . runActionWithInit Nothing PushOk
 
 -- Handle an event and simulate its side effects, then ignore the side effects
 -- and return the new state.
@@ -108,7 +113,7 @@ proceedUntilFixedPointFlat
   -> ProjectState
   -> (ProjectState, [ActionFlat])
 proceedUntilFixedPointFlat integrateResult pushResult state =
-  swap $ runActionWithInit integrateResult pushResult $ proceedUntilFixedPoint state
+  runWriter $ runActionWithInit integrateResult pushResult $ proceedUntilFixedPoint state
 
 main :: IO ()
 main = hspec $ do
