b378e202fcb62e1a78f6bfc2daaa2a5fab4c3bd5
Marcin Rzeźnicki <marcin.rzeznicki@channable.com>
2021-06-23 16:54:32 +0200

    Do not use 'fakeroot' to package 'Hoff'

--- default.nix
+++ default.nix
@@ -5,10 +5,6 @@ in
     name = "hoff-devenv";
     paths = [
       pkgs.dpkg
-      # Use `fakeroot` from the OS
-      # The version shipped with nix might not be compatible with system's libs
-      # and cause segfaults
-      # pkgs.fakeroot
       pkgs.git
       pkgs.shellcheck
       pkgs.stack
