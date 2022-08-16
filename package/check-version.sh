a84da2d86ff7108f09c8f38ad3fddb0cfeabe9e6
Rudy Matela <rudy.matela@channable.com>
2022-08-16 15:58:39 +0200

    check-version: include nix version check

--- package/check-version.sh
+++ package/check-version.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 #
-# Checks if the version number in hoff.cabal is the same as src/Version.hs
+# Checks if the version number is consistent across different files:
+# * hoff.cabal
+# * hoff.nix
+# * src/Version.hs
 #
-# This is intended to be called on Semaphore.
+# This is intended to be called on CI (Semaphore).
 #
-# One alternative to this scripts would be to add this test directly on
-# test/*.hs while imposing a dependency on Cabal-syntax there.
-# Then we could "properly" parse the Cabal file to get the version:
+# We could overengineer this by:
 #
-# https://hackage.haskell.org/package/Cabal-syntax-3.8.1.0/docs/Distribution-Types-GenericPackageDescription.html#t:GenericPackageDescription
+# * Including the check in test/Spec.hs or test/*.hs;
 #
-# But for a simple check like this, that may be overengineering.
+# * using Cabal-syntax's GenericPackageDescription function
+#   to "properly" parse the version in the cabal file;
+#
+# * similarly "properly" parsing hoff.nix thorugh Haskell.
+#
+# This is simpler, easier and effective.
 
-cabal_version=$( grep "^version:"   <hoff.cabal     | sed 's/.*version: *//;  s/ .*//')
-source_version=$(grep "^version *=" <src/Version.hs | sed 's/.*version *= *"//; s/".*//')
+cabal_version=$(
+	grep "^version:" <hoff.cabal |
+	sed 's/.*version: *//;  s/ .*//'
+)
+source_version=$(
+	grep "^version *=" <src/Version.hs |
+	sed 's/.*version *= *"//; s/".*//'
+)
+nix_version=$(
+	grep "^[\t ]*version *=" <hoff.nix |
+	sed 's/.*version *= *"//; s/".*//'
+)
 
-[ "$cabal_version" = "$source_version" ] && exit 0
+[ "$cabal_version" = "$source_version" ] && [ "$nix_version" = "$source_version" ] && exit 0
 
 cat <<MESSAGE
-mismatch in versions in hoff.cabal and src/Version.hs:
+mismatch in versions in hoff.cabal, hoff.nix and src/Version.hs:
 * hoff.cabal:     $cabal_version
+* hoff.nix:       $nix_version
 * src/Version.hs: $source_version
 MESSAGE
 exit 1
