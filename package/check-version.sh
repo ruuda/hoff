ee6a20e648eb17c9fbfd143eb910711fba44eb8a
Rudy Matela <rudy.matela@channable.com>
2022-08-16 15:32:49 +0200

    Document CI check alternative

--- package/check-version.sh
+++ package/check-version.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #
 # Checks if the version number in hoff.cabal is the same as src/Version.hs
+#
+# This is intended to be called on Semaphore.
+#
+# One alternative to this scripts would be to add this test directly on
+# test/*.hs while imposing a dependency on Cabal-syntax there.
+# Then we could "properly" parse the Cabal file to get the version:
+#
+# https://hackage.haskell.org/package/Cabal-syntax-3.8.1.0/docs/Distribution-Types-GenericPackageDescription.html#t:GenericPackageDescription
+#
+# But for a simple check like this, that may be overengineering.
 
 cabal_version=$( grep "^version:"   <hoff.cabal     | sed 's/.*version: *//;  s/ .*//')
 source_version=$(grep "^version *=" <src/Version.hs | sed 's/.*version *= *"//; s/".*//')
