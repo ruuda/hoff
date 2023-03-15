ae43e19287634829128e640f09d87085b1027f35
Curtis Chin Jen Sem <curtis.chinjensem@channable.com>
2023-03-15 16:12:18 +0100

    Add prop test for build status summaries

--- tests/ProjectSpec.hs
+++ tests/ProjectSpec.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module ProjectSpec (projectSpec) where
 
-import Test.Hspec (Spec, describe, shouldBe)
+import Control.Monad (forM_)
+import Test.Hspec (Spec, describe, shouldBe, shouldNotBe, shouldSatisfy)
 import Test.Hspec.QuickCheck (prop)
-
+import Test.QuickCheck ((==>), Arbitrary, genericShrink)
+import Test.QuickCheck.Arbitrary (arbitrary, shrink)
+import Test.QuickCheck.Arbitrary.Generic (genericArbitrary)
 import Test.QuickCheck.Instances.Text ()
 
-import qualified Project
-import Control.Monad (forM_)
 import qualified Data.Map.Strict as Map
+
+import qualified Project
 
 projectSpec :: Spec
 projectSpec = do
@@ -21,3 +26,21 @@ projectSpec = do
       forM_ owners $ \owner -> do
         Project.subMapByOwner owner projectInfoMap `shouldBe`
           Map.filterWithKey (\key _ -> Project.owner key == owner) projectInfoMap
+
+  describe "Project.summarize" $ do
+    prop "Ensure successes are always overshadowed by other statuses" $ \statuses -> do
+      let outstanding = Project.SpecificChecks $ Map.fromList statuses
+      any ((/= Project.BuildSucceeded) . snd) statuses ==> do
+        Project.summarize outstanding `shouldNotBe` Project.BuildSucceeded
+    prop "Ensure failures always overshadow other statuses" $ \statuses -> do
+      let outstanding = Project.SpecificChecks $ Map.fromList statuses
+          isFailure (Project.BuildFailed _) = True
+          isFailure _ = False
+      any (isFailure . snd) statuses ==> do
+        Project.summarize outstanding `shouldSatisfy` isFailure
+
+instance Arbitrary Project.Check where
+  arbitrary = Project.Check <$> arbitrary
+instance Arbitrary Project.BuildStatus where
+  arbitrary = genericArbitrary
+  shrink = genericShrink
