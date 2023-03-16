bc782a5cf61bc624393e6b8323551a62e82fb0c8
Curtis Chin Jen Sem <curtis.chinjensem@channable.com>
2023-03-16 16:10:12 +0100

    Fix property test not guarenteeing failure status

--- tests/ProjectSpec.hs
+++ tests/ProjectSpec.hs
@@ -7,7 +7,7 @@ module ProjectSpec (projectSpec) where
 import Control.Monad (forM_)
 import Test.Hspec (Spec, describe, shouldBe, shouldNotBe, shouldSatisfy)
 import Test.Hspec.QuickCheck (prop)
-import Test.QuickCheck ((==>), Arbitrary, genericShrink)
+import Test.QuickCheck ((==>), Arbitrary, genericShrink, withMaxSuccess)
 import Test.QuickCheck.Arbitrary (arbitrary, shrink)
 import Test.QuickCheck.Arbitrary.Generic (genericArbitrary)
 import Test.QuickCheck.Instances.Text ()
@@ -19,7 +19,7 @@ import qualified Project
 projectSpec :: Spec
 projectSpec = do
   describe "Project.subMapByOwner" $ do
-    prop "the ord instance guarentees owners are grouped together" $ \(owners, repositories) -> do
+    prop "the ord instance guarentees owners are grouped together" $ withMaxSuccess 100 $ \(owners, repositories) -> do
       let projectInfos = [Project.ProjectInfo owner repo | owner <- owners, repo <- repositories]
           projectInfoMap = Map.fromList (map (\p -> (p, Project.repository p)) projectInfos)
 
@@ -28,15 +28,17 @@ projectSpec = do
           Map.filterWithKey (\key _ -> Project.owner key == owner) projectInfoMap
 
   describe "Project.summarize" $ do
-    prop "Ensure successes are always overshadowed by other statuses" $ \statuses -> do
-      let outstanding = Project.SpecificChecks $ Map.fromList statuses
-      any ((/= Project.BuildSucceeded) . snd) statuses ==> do
+    prop "Ensure successes are always overshadowed by other statuses" $ withMaxSuccess 1000 $ \statuses -> do
+      let checkMap = Map.fromList statuses
+          outstanding = Project.SpecificChecks checkMap
+      any ((/= Project.BuildSucceeded) . snd) (Map.toList checkMap) ==> do
         Project.summarize outstanding `shouldNotBe` Project.BuildSucceeded
-    prop "Ensure failures always overshadow other statuses" $ \statuses -> do
-      let outstanding = Project.SpecificChecks $ Map.fromList statuses
+    prop "Ensure failures always overshadow other statuses" $ withMaxSuccess 1000 $ \statuses -> do
+      let checkMap = Map.fromList statuses
+          outstanding = Project.SpecificChecks checkMap
           isFailure (Project.BuildFailed _) = True
           isFailure _ = False
-      any (isFailure . snd) statuses ==> do
+      any (isFailure . snd) (Map.toList checkMap) ==> do
         Project.summarize outstanding `shouldSatisfy` isFailure
 
 instance Arbitrary Project.Check where
