94aecdcecf2254e1a90bdf32608d801524bd28f0
Hugo Peters <hugo.peters@channable.com>
2023-03-27 16:51:29 +0200

    Hoff v0.28.5

--- hoff.nix
+++ hoff.nix
@@ -4,7 +4,7 @@ let
 in
   haskellPackages.mkDerivation {
     pname = "hoff";
-    version = "0.28.4"; # please keep consistent with hoff.cabal
+    version = "0.28.5"; # please keep consistent with hoff.cabal
 
     src =
       let
